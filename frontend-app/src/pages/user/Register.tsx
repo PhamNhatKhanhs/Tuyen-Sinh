b4855cc51b4075535ee79e24b0ed64f601782d9f
Nguyá»…n Thá»‹ Háº¡nh NguyÃªn <nguyen.user.frontend@gmail.com>
2025-05-16 19:21:00 +0700

    init: create Register structure - user interface

---
+++ frontend-app/src/pages/user/Register.tsx
@@ -0,0 +1,25 @@
+﻿/**
+ * Register Component
+ * Author: Nguyá»…n Thá»‹ Háº¡nh NguyÃªn
+ * Created: 2025-06-11
+ */
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+interface RegisterProps {
+  // TODO: Define props
+}
+
+const RegisterComponent: React.FC<RegisterProps> = () => {
+  // TODO: Implement component logic
+  
+  return (
+    <div className="register">
+      <h1>Register</h1>
+      {/* TODO: Add component content */}
+    </div>
+  );
+};
+
+export default RegisterComponent;
