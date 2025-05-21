6706b6ced9c33a73b6da81bc004290ba12555890
Nguyá»…n Thá»‹ Háº¡nh NguyÃªn <nguyen.user.frontend@gmail.com>
2025-05-21 19:44:00 +0700

    hotfix: UniversitySelector problem - user interface

--- frontend-app/src/components/user/UniversitySelector.tsx
+++ frontend-app/src/components/user/UniversitySelector.tsx
@@ -2,3 +2,7 @@
 // Update by Nguyá»…n Thá»‹ Háº¡nh NguyÃªn - 2025-06-11 02:35
 // update: Enhanced UniversitySelector component
 
+
+// Update by Nguyá»…n Thá»‹ Háº¡nh NguyÃªn - 2025-06-11 02:35
+// fix: Enhanced UniversitySelector component
+
