49cd9c33775d068a3b302ce54e6a7b6364d012ad
Pháº¡m Nháº­t KhÃ¡nh <khanh.backend.dev@gmail.com>
2025-05-31 08:07:00 +0700

    style: enhance authMiddleware appearance - backend API

--- backend-app/src/middlewares/authMiddleware.js
+++ backend-app/src/middlewares/authMiddleware.js
@@ -13,3 +13,7 @@ module.exports = router;
 // Update by Pháº¡m Nháº­t KhÃ¡nh - 2025-06-11 02:34
 // fix: Enhanced authMiddleware functionality
 
+
+// Update by Pháº¡m Nháº­t KhÃ¡nh - 2025-06-11 02:34
+// style: Enhanced authMiddleware functionality
+
