a67ecf9bb40d975246ce60704cc9a180c615c385
Nguyá»…n Thá»‹ Háº¡nh NguyÃªn <nguyen.user.frontend@gmail.com>
2025-06-07 18:00:00 +0700

    style: improve UniversitySelector UI - user interface

--- frontend-app/src/components/user/UniversitySelector.tsx
+++ frontend-app/src/components/user/UniversitySelector.tsx
@@ -10,3 +10,7 @@
 // Update by Nguyá»…n Thá»‹ Háº¡nh NguyÃªn - 2025-06-11 02:35
 // feature: Enhanced UniversitySelector component
 
+
+// Update by Nguyá»…n Thá»‹ Háº¡nh NguyÃªn - 2025-06-11 02:35
+// style: Enhanced UniversitySelector component
+
