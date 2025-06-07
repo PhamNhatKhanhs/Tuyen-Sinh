c5c5e3cc428fdb6589cc5fff60077224cd6cffac
Pháº¡m Nháº­t KhÃ¡nh <khanh.backend.dev@gmail.com>
2025-06-07 11:50:00 +0700

    bugfix: fix authMiddleware error - backend API

--- backend-app/src/middlewares/authMiddleware.js
+++ backend-app/src/middlewares/authMiddleware.js
@@ -21,3 +21,7 @@ module.exports = router;
 // Update by Pháº¡m Nháº­t KhÃ¡nh - 2025-06-11 02:34
 // fix: Enhanced authMiddleware functionality
 
+
+// Update by Pháº¡m Nháº­t KhÃ¡nh - 2025-06-11 02:34
+// fix: Enhanced authMiddleware functionality
+
